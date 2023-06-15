9c0328fd691cbd3484b9cde1a36a629d07dd7057
Dhara <dhara00719@gmail.com>
2023-06-15 16:02:27 +0530

    solve issue

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-// const { error } = require('console');
-// const express = require('express');
-// const fs = require('fs');
+{
+    // const { error } = require('console');
+    // const express = require('express');
+    // const fs = require('fs');
 
 
-// const app = express();
-// const PORT = 5000;
+    // const app = express();
+    // const PORT = 5000;
 
-// app.get('/expenses/:_userName', (req, res)=>{
-    
-//     fs.readFile("./data/expenses.json", 'utf8', (error, data) =>
-//     {      
-//         if(error){
-//             res.send(error);
-//              return;
-//         }
-        
-//         let expenses = JSON.parse(data);
-//         let userName = req.params._userName;
+    // app.get('/expenses/:_userName', (req, res)=>{
 
-//         let response = {};
-       
-//         if(expenses[userName] != undefined)
-//         {
-//             response.success = true;
-//             response.data = expenses[userName];
+    //     fs.readFile("./data/expenses.json", 'utf8', (error, data) =>
+    //     {      
+    //         if(error){
+    //             res.send(error);
+    //              return;
+    //         }
 
-//             res.send( 200, response);
-//         }else{
-//             response.success = false;
-//             response.message = "user not found";
-//             res.send(404, response);
-//         }
-//     });
-    
-// });
+    //         let expenses = JSON.parse(data);
+    //         let userName = req.params._userName;
 
-// app.listen(PORT);
+    //         let response = {};
 
+    //         if(expenses[userName] != undefined)
+    //         {
+    //             response.success = true;
+    //             response.data = expenses[userName];
 
+    //             res.send( 200, response);
+    //         }else{
+    //             response.success = false;
+    //             response.message = "user not found";
+    //             res.send(404, response);
+    //         }
+    //     });
+
+    // });
+
+    // app.listen(PORT);
+}
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes/api');
 const mongoose = require('mongoose');
 
-const DB_CONNECTION = "mongodb://127.0.0.1:27017";
-
-mongoose.connect(DB_CONNECTION, {
-    autoIndex: true});
+mongoose.connect(process.env.DB_CONNECTION, {
+    autoIndex: true
+});
 
 const database = mongoose.connection
 
@@ -58,20 +58,19 @@ database.once('connected', () => {
 })
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use('/expenses/api', routes)
 
-app.use(function(error,req,res,next){
+app.use(function (error, req, res, next) {
     //console.log('Path: ', req.path)
     console.error('Error: ', error)
-    
 
-    if(error.code == 11000) 
-    {
-        handleDuplicateKeyError(error,res);
-    }else{
+
+    if (error.code == 11000) {
+        handleDuplicateKeyError(error, res);
+    } else {
         res.status(422).send(error);
     }
 });
@@ -86,71 +85,72 @@ const handleDuplicateKeyError = (err, res) => {
     const code = 409;
     const message = `An account with that ${field} already exists.`;
 
-    res.status(code).send({messages: message, fields: field});
- }
+    res.status(code).send({ messages: message, fields: field });
+}
 
-//-------------------------------------------------------------------
+{
+    //-------------------------------------------------------------------
 
-// let obj1 = {
-//     "a" : "1",
-//     "b" : 2,
-//     "c" : 2
-// }
+    // let obj1 = {
+    //     "a" : "1",
+    //     "b" : 2,
+    //     "c" : 2
+    // }
 
-// const swap = ()=>
-// {
-// //   const val =   [1,2, 3].reduce((acc, cur) => {
+    // const swap = ()=>
+    // {
+    // //   const val =   [1,2, 3].reduce((acc, cur) => {
 
-// //     console.log(acc)
-// //         return [ cur, ...acc ]
-// //     }, [])
+    // //     console.log(acc)
+    // //         return [ cur, ...acc ]
+    // //     }, [])
 
-// //     console.log(val);
-//  //============================================
-//     // const res = {};
+    // //     console.log(val);
+    //  //============================================
+    //     // const res = {};
 
-//     // for (let key in obj1) {
+    //     // for (let key in obj1) {
 
-//     //     if(obj1[key] in res)
-//     //     {
-//     //         if(typeof res[obj1[key]] != "Array" )
-//     //         {
-//     //             res[obj1[key]] = Array.from(res[obj1[key]]);
-//     //         }
-//     //         res[obj1[key]].push(key);
-            
-//     //     }else{
-//     //         res[obj1[key]] = key;
-//     //     }       
+    //     //     if(obj1[key] in res)
+    //     //     {
+    //     //         if(typeof res[obj1[key]] != "Array" )
+    //     //         {
+    //     //             res[obj1[key]] = Array.from(res[obj1[key]]);
+    //     //         }
+    //     //         res[obj1[key]].push(key);
 
-//     // }
+    //     //     }else{
+    //     //         res[obj1[key]] = key;
+    //     //     }       
 
-//     // console.log(res);
-// //======================================
-//     let res1 = Object.entries(obj1).reduce((prev, curr) => 
-//     {
-//         if(curr[1] in prev)
-//         {
-//             if(typeof prev[curr[1]] != "Array" )
-//             {
-//                 prev[curr[1]] = Array.from(prev[curr[1]]);
-//             }
-//             prev[curr[1]].push(curr[0]);
-            
-//         }else{
-//             prev[curr[1]] = curr[0];
-//         }    
+    //     // }
 
-//         return prev;
-//     },{});
+    //     // console.log(res);
+    // //======================================
+    //     let res1 = Object.entries(obj1).reduce((prev, curr) => 
+    //     {
+    //         if(curr[1] in prev)
+    //         {
+    //             if(typeof prev[curr[1]] != "Array" )
+    //             {
+    //                 prev[curr[1]] = Array.from(prev[curr[1]]);
+    //             }
+    //             prev[curr[1]].push(curr[0]);
 
-//    console.log(res1);
+    //         }else{
+    //             prev[curr[1]] = curr[0];
+    //         }    
 
-// }
+    //         return prev;
+    //     },{});
 
-// //console.log(obj1);
-// swap();
+    //    console.log(res1);
 
-//------------------------------------------------------------------------------------
+    // }
 
+    // //console.log(obj1);
+    // swap();
+
+    //------------------------------------------------------------------------------------
+}
 
