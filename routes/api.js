9ef2ce2e599972276a12f46575d85b68a322b452
Dhara <dhara00719@gmail.com>
2023-06-15 16:11:52 +0530

    added vercel file

--- routes/api.js
+++ routes/api.js
@@ -5,134 +5,172 @@ const Account = require('../model/account');
 
 const router = express.Router()
 
-// //Post Method
-// router.post('/post', (req, res, next) => {
-//     res.send('Post API...' +   JSON.stringify(req.body));
+const ADD = "add";
+const GET_ALL = "getAll";
+const GET_ONE = "getOne";
+const EDIT_ONE = "editOne";
+const DELETE_ONE = "deleteOne";
 
-//     const data = new Expense()
+const EXPENSES = "expenses";
+const SOURCE = "source";
+const ACCOUNT = "account";
 
-//     // Expense.create(req.body).then(function(data){
-//     //     res.send(data);
-//     // }).catch(next);
-// })
 
-// //Get all Method
-// router.get('/getAll', (req, res) => {
-//     res.send('Get All API');
+// CURD Demo
+{
+    // //Post Method
+    // router.post('/post', (req, res, next) => {
+    //     res.send('Post API...' +   JSON.stringify(req.body));
 
-//     // Expense.find({}).then(function(data){
-//     //     res.send(data);
-//     // }).catch(next);
-// })
+    //     const data = new Expense()
 
-// //Get by ID Method
-// router.get('/getOne/:id', (req, res) => {
-//     res.send('Get by ID API')
-// })
+    //     // Expense.create(req.body).then(function(data){
+    //     //     res.send(data);
+    //     // }).catch(next);
+    // })
 
-// //Update by ID Method
-// router.patch('/update/:id', (req, res) => {
-//     res.send('Update by ID API')
-// })
+    // //Get all Method
+    // router.get('/getAll', (req, res) => {
+    //     res.send('Get All API');
 
-// //Delete by ID Method
-// router.delete('/delete/:id', (req, res) => {
-//     res.send('Delete by ID API')
-// })
+    //     // Expense.find({}).then(function(data){
+    //     //     res.send(data);
+    //     // }).catch(next);
+    // })
 
+    // //Get by ID Method
+    // router.get('/getOne/:id', (req, res) => {
+    //     res.send('Get by ID API')
+    // })
+
+    // //Update by ID Method
+    // router.patch('/update/:id', (req, res) => {
+    //     res.send('Update by ID API')
+    // })
+
+    // //Delete by ID Method
+    // router.delete('/delete/:id', (req, res) => {
+    //     res.send('Delete by ID API')
+    // })
+}
 
 // CURD In account collection
+{
 
-//Post Method
-router.post('/account/add', async (req, res, next) => {
+    //Post Method
+    router.post(`/${ACCOUNT}/${ADD}`, async (req, res, next) => {
 
-    const data = new Account(req.body);
+        const data = new Account(req.body);
 
-    data.save()
-        .then(response => res.send(response))
-        .catch(error => next (error));
-})
+        data.save()
+            .then(response => res.send(response))
+            .catch(error => next(error));
+    })
 
-//Get All
-router.get('/account/getAll', (req, res, next) => {
-    Account.find()
-        .then(response => res.send(response))
-        .catch(error => next (error));
-})
+    //Get All
+    router.get(`/${ACCOUNT}/${GET_ALL}`, (req, res, next) => {
+        Account.find()
+            .then(response => res.send(response))
+            .catch(error => next(error));
+    })
 
-//Get by ID Method
-router.get('/account/getOne/:id', (req, res, next) => {
-    Account.findById(req.params.id)
-        .then(response => res.send(response))
-        .catch(error => next (error));
-})
+    //Get by ID Method
+    router.get(`/${ACCOUNT}/${GET_ONE}/:id`, (req, res, next) => {
+        Account.findById(req.params.id)
+            .then(response => res.send(response))
+            .catch(error => next(error));
+    })
+
+}
 
 // CURD In source collection
+{
 
-//Post Method
-router.post('/source/add', async (req, res, next) => {
+    //Post Method
+    router.post(`/${SOURCE}/${ADD}`, async (req, res, next) => {
 
-    const data = new Source(req.body);
+        const data = new Source(req.body);
 
-    data.save()
-        .then(response => res.send(response))
-        .catch(error => next (error));
-})
-
-//Get All
-router.get('/source/getAll', (req, res, next) => {
-    Source.find()
-    .populate('account_id')
-    .then((response) => {
-        console.log(response);
-        res.send(response);
+        data.save()
+            .then(response => res.send(response))
+            .catch(error => next(error));
     })
-    .catch(error => next (error));
-})
+
+    //Get All
+    router.get(`/${SOURCE}/${GET_ALL}`, (req, res, next) => {
+        Source.find()
+            .populate('account_id')
+            .then((response) => {
+                console.log(response);
+                res.send(response);
+            })
+            .catch(error => next(error));
+    })
+
+}
 
 // CURD In expenses collection
+{
+    //Post Method
+    router.post(`/${EXPENSES}/${ADD}`, async (req, res, next) => {
 
-//Post Method
-router.post('/expenses/add', async (req, res, next) => {
+        const data = new Expense(req.body);
 
-    const data = new Expense(req.body);
-
-    data.save()
-        .then(response => res.send(response))
-        .catch(error => next (error));
-})
-
-//Get All
-router.get('/expenses/getAll', (req, res, next) => {
-    Expense.find()
-    .populate({
-        path: 'source_id',
-        model: Source,
-        populate: {
-           path: 'account_id',
-           model: Account
-        }
-     })
-    .then((response) => {
-        console.log(response);
-        res.send(response);
+        data.save()
+            .then(response => res.send(response))
+            .catch(error => next(error));
     })
-    .catch(error => next (error));
-})
 
-//Get by ID Method
-router.get('/expenses/getOne/:id', (req, res, next) => {
-    Expense.findById(req.params.id)
-        .populate({
-            path: 'source_id',
-            model: Source,
-            populate: {
-               path: 'account_id',
-               model: Account
-            }
-         })
-        .then(response => res.send(response))
-        .catch(error => next (error));
-})
+    //Get All
+    router.get(`/${EXPENSES}/${GET_ALL}`, (req, res, next) => {
+        Expense.find()
+            .populate({
+                path: 'source_id',
+                model: Source,
+                populate: {
+                    path: 'account_id',
+                    model: Account
+                }
+            })
+            .then((response) => {
+                console.log(response);
+                res.send(response);
+            })
+            .catch(error => next(error));
+    })
+
+    //Get by ID Method
+    router.get(`/${EXPENSES}/${GET_ONE}/:id`, (req, res, next) => {
+        Expense.findById(req.params.id)
+            .populate({
+                path: 'source_id',
+                model: Source,
+                populate: {
+                    path: 'account_id',
+                    model: Account
+                }
+            })
+            .then(response => res.send(response))
+            .catch(error => next(error));
+    })
+
+    // Delete by ID Method
+    router.delete(`/${EXPENSES}/${DELETE_ONE}/:id`, (req, res, next) => {
+
+        Expense.findByIdAndDelete(req.params.id)
+            .then(response => res.send(response))
+            .catch(error => next(error));
+        //res.send('Delete by ID API')
+    })
+
+    // Edit by ID Method
+    router.delete(`/${EXPENSES}/${EDIT_ONE}/:id`, (req, res, next) => {
+
+        Expense.findByIdAndUpdate(req.params.id, new Expense(req.body))
+            .then(response => res.send(response))
+            .catch(error => next(error));
+        //res.send('Delete by ID API')
+    })
+}
 
 module.exports = router;
